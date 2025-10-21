8aac6c870e5955a2e16aa5962af16a2d3cbf8c90
DitteAndersen <dian1001@stud.ek.dk>
2025-10-21 11:28:34 +0200

    Add files via upload

--- menu.js
+++ menu.js
@@ -6,3 +6,37 @@ function burgerClick() {
   burger.classList.toggle("active");
   nav.classList.toggle("active");
 }
+document.addEventListener("DOMContentLoaded", () => {
+  if (document.body.classList.contains("home")) {
+    const header = document.querySelector("header");
+    const hero = document.querySelector(".hero");
+
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (!entry.isIntersecting) {
+            header.classList.add("scrolled");
+          } else {
+            header.classList.remove("scrolled");
+          }
+        });
+      },
+      { threshold: 0.1 }
+    );
+
+    observer.observe(hero);
+  }
+});
+document.addEventListener("DOMContentLoaded", () => {
+  if (!document.body.classList.contains("home")) {
+    const header = document.querySelector("header");
+
+    window.addEventListener("scroll", function () {
+      if (window.scrollY > 60) {
+        header.classList.add("scrolled");
+      } else {
+        header.classList.remove("scrolled");
+      }
+    });
+  }
+});
